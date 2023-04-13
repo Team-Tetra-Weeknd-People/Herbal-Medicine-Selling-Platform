89f4b57955201c9bdb247c182cec17bc0510c401
Chanudi Tharushika <87629005+IT20029586@users.noreply.github.com>
2023-04-13 13:00:43 +0530

    Admin API implemented

---
+++ backend/UserService/models/admin.js
@@ -0,0 +1,81 @@
+import mongoose from 'mongoose';
+import bcrypt from 'bcrypt';
+
+const Schema = mongoose.Schema;
+
+const adminSchema = new Schema({
+
+    firstName: {
+        type: String,
+        required: true
+    },
+
+    lastName: {
+        type: String,
+        required: true
+    },
+
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+
+    contactNo: {
+        type: String,
+        required: true
+    },
+
+    password: {
+        type: String,
+        required: true
+    },
+
+    image: {
+        type: String,
+        required: false
+    },
+
+    token: {
+        type: Number,
+        required: false,
+        default: Math.floor(Math.random() * 1000000)
+    },
+
+    resetToken: {
+        type: Number,
+        required: false,
+        default: 0
+    },
+
+    verified: {
+        type: Boolean,
+        required: false,
+        default: false
+    }
+
+}, {
+    timestamps: {
+        createdAt: 'createdOn',
+        updatedAt: 'updatedOn'
+    }
+});
+
+adminSchema.pre("save", async function (next) {
+    const user = this;
+    const password = user.password;
+
+    if (!user.isModified("password")) {
+        return next();
+    }
+
+    // Number of rounds hash function will execute
+    const salt = await bcrypt.genSalt(10);
+
+    const hash = bcrypt.hashSync(password, salt);
+    user.password = hash;
+    return next();
+});
+
+const Admin = mongoose.model('Admin', adminSchema);
+export default Admin;
