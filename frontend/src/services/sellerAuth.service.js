48e3fbaead852582aa4cea69564535925f6951ca
Randula Mawaththta <randula98@gmail.com>
2023-04-14 02:47:19 +0530

    Seller Dashboard Implemented

--- frontend/src/services/sellerAuth.service.js
+++ frontend/src/services/sellerAuth.service.js
@@ -17,8 +17,44 @@ const login = async (data) => {
     });
 }
 
+const getCurrentUser = (id) => {
+    return axios.get(url.GET_SELLER(id), {
+        headers: {
+            "Content-Type": "application/json"
+        },
+    });
+}
+
+const updateSeller = (id, data) => {
+    return axios.put(url.UPDATE_SELLER(id), data, {
+        headers: {
+            "Content-Type": "application/json"
+        },
+    });
+}
+
+const deleteSeller = (id) => {
+    return axios.delete(url.DELETE_SELLER(id), {
+        headers: {
+            "Content-Type": "application/json"
+        },
+    });
+}
+
+const getAllSellers = () => {
+    return axios.get(url.GET_ALL_SELLERS, {
+        headers: {
+            "Content-Type": "application/json"
+        },
+    });
+}
+
 // eslint-disable-next-line import/no-anonymous-default-export
 export default {
     register,
-    login
+    login,
+    getCurrentUser,
+    updateSeller,
+    deleteSeller,
+    getAllSellers
 }
