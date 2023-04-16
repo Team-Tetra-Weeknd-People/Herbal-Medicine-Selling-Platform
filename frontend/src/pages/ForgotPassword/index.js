8cab568b25ea022c32a8b5fed13f482c305663f0
Chanudi Tharushika <87629005+IT20029586@users.noreply.github.com>
2023-04-16 22:11:59 +0530

    forgot pw and update pw ui added

--- frontend/src/pages/ForgotPassword/index.js
+++ frontend/src/pages/ForgotPassword/index.js
@@ -4,22 +4,23 @@ import Form from 'react-bootstrap/Form';
 //import Row from 'react-bootstrap/Row';
 //import Col from 'react-bootstrap/Col';
 
-import './UpdatePassword.css'
+import './ForgotPassword.css'
 
 export default function UpdatePass() {
     return (
         <>
-            <div className='upcontainer'>
+            <div className='fcontainer'>
                 <Form>
                     <Form.Group className="mb-3" controlId="formUP">
-                        <Form.Label className='label'>Update Password</Form.Label>
+                        <Form.Label className='label'>Forgot Password</Form.Label>
                         <br></br>
-                        <Form.Control type="text" className='password' placeholder="Enter New Password" />
-                        {/* <Form.Text className="text-muted">
+                        <div className='pwcontainer'>
+                            <Form.Control type="text" className='password' placeholder="Enter New Password" />
+                        </div>
+                        <Form.Text className="text-muted">
                             We'll never share your email with anyone else.
-                        </Form.Text> */}
+                        </Form.Text>
                         <br></br>
-                        <Form.Control type="text" className='password' placeholder="Confirm Password" />
                     </Form.Group>
 
                     <Button type="submit" className="button">
