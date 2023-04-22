f21d8a7fd353e1551955f270bb219eb976163502
Randula Mawaththta <randula98@gmail.com>
2023-04-23 00:30:10 +0530

    Order API Fixed

--- backend/OrderService/controllers/cartItem.js
+++ backend/OrderService/controllers/cartItem.js
@@ -67,7 +67,7 @@ export const getCartItemsByCartID = async (req, res) => {
 export const getRecentOrdersByBrand = async (req, res) => {
     const brand = req.params.brand;
     try {
-        const carts = await CartItem.find({ brand: brand }).sort({ createdAt: -1 }).limit(9);
+        const carts = await CartItem.find({ itembrand: brand }).sort({ createdAt: -1 }).limit(8);
         res.status(200).json(carts);
     } catch (error) {
         res.status(409).json({ message: error.message });
