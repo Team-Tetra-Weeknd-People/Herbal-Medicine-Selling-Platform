2fe485d2e331303064533f9eb8dc60b48346660b
IT21008382 <it21008382@my.sliit.lk>
2023-04-26 11:38:13 +0530

    footer implementation

--- frontend/src/components/footer/index.js
+++ frontend/src/components/footer/index.js
@@ -1,7 +1,17 @@
 import React from 'react';
+import '../../styles/madusha/footer.css';
 
 
 export default function footer(){
-   
-    
+    <div className="wrapper">
+    <div className="content-1">
+      <h1>All rights reserved</h1>
+    </div>
+    <div className="content-2">
+      <h1>@Beheth kade 2023</h1>
+    </div>
+    <div className="footer">
+      <h2>Sticky Footer</h2>
+    </div>
+  </div>
 }
